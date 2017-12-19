536d45b1a337e38092633a5195e699946e3f59b3
idovgalyov <idovgalyov@4xxi.com>
2017-12-19 21:25:36 +0300

    feature/refactoring WIP (2)
    Stable version including:
    -- config.py for configuration parameters
    -- sourcelib.py for downloading/creating source data files
    -- processlib.py including methods for getting statistics and dumping results
    -- resultlib.py configuring paths to results/ directory
    -- main.py as executable file
    
    Still need to provide README and docstrings.

--- KNN/methods.py
+++ KNN/methods.py
@@ -1,6 +1,26 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+import numpy as np
 
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import recall_score, precision_score, accuracy_score
 from sklearn.metrics import confusion_matrix
+
+
+ALLOWED_METHODS = (KNeighborsClassifier, GradientBoostingClassifier)
+
+
+def zappala_metric(x, y, k1 = 1.25, k2 = 2.0, k3 = 2.0):
+    '''
+    Realization of a distance function defined by Zappala et al.
+    See e.g. http://adsabs.harvard.edu/abs/1990AJ....100.2030Z
+    
+    "x" and "y" are the vectors in the (a, e, sinI, n) phase space.
+    '''
+
+    return ((x[0] + y[0]) * (x[3] + y[3]) / 4.0 *
+            np.sqrt(
+            4.0 * k1 * ((x[0] - y[0]) / (x[0] + y[0]))**2 +
+            k2 * (x[1] - y[1])**2 +
+            k3 * (x[2] - y[2])**2)
+            )
 
--- KNN/sourcelib.py
+++ KNN/sourcelib.py
@@ -3,15 +3,40 @@ import urllib
 
 import pandas as pd
 
-
+SOURCE_DIRECTORY = "source/"
 FAMILY_URL = "http://hamilton.dm.unipi.it/~astdys2/propsynth/all_tro.members"
-FAMILY_SOURCE = "source/all_tro.members"
 ELEMENT_URL = "http://hamilton.dm.unipi.it/~astdys2/propsynth/all.syn"
-ELEMENT_SOURCE = "source/all.syn"
-DATA_SOURCE = "source/data.csv"
+FAMILY_SOURCE = SOURCE_DIRECTORY + "all_tro.members"
+ELEMENT_SOURCE = SOURCE_DIRECTORY + "all.syn"
+DATA_SOURCE = SOURCE_DIRECTORY + "data.csv"
+
+FAMILY_NUMBERS = []
 
 
-def load_save():
+def check_source():
+
+    try:
+        f = open(FAMILY_SOURCE, 'r')
+        f.close()
+        f = open(ELEMENT_SOURCE, 'r')
+        f.close()
+    except OSError:
+        yes = 'y' in input('Some source data have not been found. Try to download?(yes/no)').lower()
+        if yes:
+            source_load_save()
+        else:
+            print('No available source data provided. Terminating the program.')
+            raise SystemExit
+
+    try:
+        f = open(DATA_SOURCE, 'r')
+        f.close()
+    except OSError:
+        print('Working data source not found and will be created automatically.')
+        data_create()
+
+
+def source_load_save():
 
     starttime = datetime.now()
     urllib.request.urlretrieve(FAMILY_URL, FAMILY_SOURCE)
@@ -22,27 +47,21 @@ def load_save():
     print('Proper synthetic elements database downloaded. Total time passed: ', (datetime.now() - starttime).total_seconds())
 
 
-def check_source():
-
-    try:
-        f = open(FAMILY_SOURCE,'r')
-        f.close()
-        f = open(ELEMENT_SOURCE,'r')
-    except OSError:
-        yes = 'y' in input('Some source data have not been found. Try to download?(yes/no)').lower()
-        if yes:
-            load_save()
-        else:
-            print('No available source data provided. Terminating the program.')
-            raise SystemExit
-
-
 def data_create():
 
+    starttime = datetime.now()
     df1 = pd.read_csv(FAMILY_SOURCE, delim_whitespace = True, skiprows = [0], header = None, index_col = 0, usecols = [0, 2, 3], names = ['id', 'status', 'family'])
     df2 = pd.read_csv(ELEMENT_SOURCE, delim_whitespace = True, skiprows = [0, 1], header = None, index_col = 0, usecols = [0, 2, 3, 4, 5], names = ['id', 'a', 'e', 'sinI', 'n'])
 
     result = pd.concat([df2, df1], axis=1, join_axes = [df2.index]).select(lambda x: str(x).isdigit())
     result[['status', 'family']] = result[['status', 'family']].fillna(0).astype(int)
+
     result.to_csv(DATA_SOURCE, float_format = '%.7f')
+    print('Working data source successfully created. Total time passed: ', (datetime.now() - starttime).total_seconds())
+
+
+def get_possible_families(data):
+
+    global FAMILY_NUMBERS
+    FAMILY_NUMBERS = sorted(list(data.family.unique()))
 
